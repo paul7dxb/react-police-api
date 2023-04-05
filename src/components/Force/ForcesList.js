dda723ed3d00f07bd07161ae0c230fe11f7aff8e
paul7dxb <62883464+paul7dxb@users.noreply.github.com>
2023-04-05 16:19:52 +0100

    Filter Lists added

--- src/components/Force/ForcesList.js
+++ src/components/Force/ForcesList.js
@@ -5,12 +5,13 @@
 import { getAllForces } from "../../util/AllForces";
 import { useState, useEffect, useCallback } from "react";
 import ForcesListItem from "./ForcesListItem";
-import classes from "./ForcesList.module.css"
+import classes from "./ForcesList.module.css";
 
 const ForcesList = () => {
 	const [forcesData, setForcesData] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [apiError, setapiError] = useState(null);
+	const [forceFilter, setForceFilter] = useState("");
 
 	const fetchForcesListHandler = useCallback(async () => {
 		try {
@@ -27,19 +28,38 @@ const ForcesList = () => {
 		fetchForcesListHandler();
 	}, [fetchForcesListHandler]);
 
+	console.log(forceFilter);
+
 	return (
-		<div className={classes.forceListContainer}>
-			{forcesData
-				? forcesData.map((elem) => (
-						<ForcesListItem
-							key={elem["id"]}
-							forceID={elem["id"]}
-							forceName={elem["name"]}
-						/>
-				  ))
-				: undefined}
-			{apiError ? <h2>{apiError}</h2> : undefined}
-		</div>
+		<>
+			<input
+			className={classes.forceFilterInput}
+				id="filter"
+				name="filter"
+				type="text"
+				placeholder="Filter Forces"
+				// value={filter}
+				onChange={(event) =>
+					setForceFilter(event.target.value.toUpperCase())
+				}
+			/>
+			<div className={classes.forceListContainer}>
+				{forcesData
+					? forcesData
+							.filter((elem) =>
+								elem["name"].toUpperCase().includes(forceFilter)
+							)
+							.map((elem) => (
+								<ForcesListItem
+									key={elem["id"]}
+									forceID={elem["id"]}
+									forceName={elem["name"]}
+								/>
+							))
+					: undefined}
+				{apiError ? <h2>{apiError}</h2> : undefined}
+			</div>
+		</>
 	);
 };
 export default ForcesList;
--- src/components/Force/NeighbourhoodList.js
+++ src/components/Force/NeighbourhoodList.js
@@ -1,22 +1,47 @@
 import { Link, useParams } from "react-router-dom";
 import classes from "./NeighbourhoodList.module.css";
+import { useState } from "react";
 
 const NeighbourhoodList = (props) => {
 	const neighbourhoodData = props.neighbourhoodData;
 	const params = useParams();
+	const [neighbourhoodFilter, setNeighbourhoodFilter] = useState("");
 
 	console.log(params);
 
 	return (
-		<div className={classes.neighbourhoodListContainer}>
-			{neighbourhoodData
-				? neighbourhoodData.map((elem) => (
-						<Link className={classes.linkCard} key={elem.id} to={elem.id}>
-							{elem.name}
-						</Link>
-				  ))
-				: undefined}
-		</div>
+		<>
+			<input
+				className={classes.neighbourhoodFilterInput}
+				id="filter"
+				name="filter"
+				type="text"
+				placeholder="Filter Neighbourhood"
+				// value={filter}
+				onChange={(event) =>
+					setNeighbourhoodFilter(event.target.value.toUpperCase())
+				}
+			/>
+			<div className={classes.neighbourhoodListContainer}>
+				{neighbourhoodData
+					? neighbourhoodData
+							.filter((elem) =>
+								elem.name
+									.toUpperCase()
+									.includes(neighbourhoodFilter)
+							)
+							.map((elem) => (
+								<Link
+									className={classes.linkCard}
+									key={elem.id}
+									to={elem.id}
+								>
+									{elem.name}
+								</Link>
+							))
+					: undefined}
+			</div>
+		</>
 	);
 };
 
