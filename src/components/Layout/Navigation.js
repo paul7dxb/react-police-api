aecea63bd7b411d0907e4b6bb70bce6521def264
paul7dxb <62883464+paul7dxb@users.noreply.github.com>
2023-04-08 13:33:23 +0100

    Responsive navbar

--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -1,40 +1,65 @@
-import { NavLink } from "react-router-dom"
-import classes from "./Navigation.module.css"
+import { NavLink } from "react-router-dom";
+import classes from "./Navigation.module.css";
+import { useState } from "react";
+// import hamburger from "../assets/icons/hamburger.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navigation = () => {
-return (
-    <header className={classes.header}>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/" className={({isActive}) => isActive ? classes.active : undefined} >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/forces" className={({isActive}) => isActive ? classes.active : undefined} >
-                        Forces
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/search" className={({isActive}) => isActive ? classes.active : undefined} >
-                        Search
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/more-info" className={({isActive}) => isActive ? classes.active : undefined} >
-                        More Info
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/testground" className={({isActive}) => isActive ? classes.active : undefined} >
-                        TestGroundPage
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
-)
-}
+	const [showNavbar, setShowNavbar] = useState(false);
 
-export default Navigation
+	const handleShowNavbar = () => {
+		setShowNavbar(!showNavbar);
+	};
+
+	return (
+		<header className={classes.header}>
+			<GiHamburgerMenu className={classes.menuIcon} onClick={handleShowNavbar}/>
+			<nav >
+				<ul className={showNavbar? classes.navbarActive : ""}>
+					<li>
+						<NavLink
+							to="/"
+							className={({ isActive }) =>
+								isActive ? classes.active : undefined
+							}
+						>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/forces"
+							className={({ isActive }) =>
+								isActive ? classes.active : undefined
+							}
+						>
+							Forces
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/search"
+							className={({ isActive }) =>
+								isActive ? classes.active : undefined
+							}
+						>
+							Search
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/more-info"
+							className={({ isActive }) =>
+								isActive ? classes.active : undefined
+							}
+						>
+							More Info
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Navigation;
